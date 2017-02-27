245657410c1d474031363047ed6abdcf2964cfcf
rrjoshi92 <rrjoshi92@gmail.com>
2017-02-26 21:23:15 -0800

    initial commit.

---
+++ script/script.js
@@ -0,0 +1,22 @@
+﻿// Code goes here
+
+var app = angular.module("app", []);
+
+app.controller('MainController', ['$scope','$http', function ($scope, $http) {
+    $http.get('data.json').then(function (data) {
+        $scope.cards = data.data;       
+    });
+}]);
+
+app.directive('mydirective', function () {
+    return {
+        templateUrl: 'template.html',
+        scope: {
+            card: '='
+        },
+        controller: function ($scope) {
+           
+           
+        }
+    };
+});
